a7d79582b2ad46d59f6de95cdce8071af71f1af4
Paola Suarez <paolasuarez86@gmail.com>
2023-08-25 17:55:16 -0500

    politica de privacidad

--- src/components/LlamadasAccion.jsx
+++ src/components/LlamadasAccion.jsx
@@ -12,6 +12,9 @@ const LlamadasAccion = () => {
             data-aos-easing="linear"
             data-aos-duration="1500" class="py-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-strong-pink dark:text-white mb-2">Descárgate nuestra aplicación ahora</h2>
           <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Eleva tu autoestima y comienza a vivir como habías anhelado</p>
+          <p className=" text-base text-gray-500" data-aos="fade-up" data-aos-delay="200">
+              *Si tienes un producto apple puedes usar la versión web
+            </p>
           <div class="lg:mt-8 items-center justify-center lg:justify-left space-x-4  flex flex-col lg:flex-row">
             <a href="#" className="container mt-12 lg:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
               <svg className="mr-3 w-16 lg:w-7 h-8" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path></svg>
--- src/components/PoliticaDePrivacidad.jsx
+++ src/components/PoliticaDePrivacidad.jsx
@@ -1,64 +1,58 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from 'react';
 
-const PoliticaDePrivacidad = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
-
+const PoliticaPrivacidad = () => {
   return (
-    <div className="max-w-screen-lg mx-auto p-8">
+    <div style={{ padding: '16px' }}>
+      <h2 style={{ fontSize: '28px', fontWeight: 'bold', color: '#E91E63' }}>Política de Privacidad</h2>
+      <p><strong>Última actualización: 3 de agosto de 2023.</strong></p>
+      <p>
+        En Una Vida Brillante, nos comprometemos a proteger su privacidad. Esta Política de Privacidad explica cómo recopilamos, usamos, compartimos y protegemos los datos personales que proporciona en nuestra app.
+      </p>
 
-      <h1 class="text-4xl font-bold mb-4">Política de Privacidad</h1>
+      {sectionTitle('Información que recopilamos')}
+      {sectionContent('Cuando se registra en nuestra app, le solicitamos su nombre, dirección de correo electrónico, ubicación aproximada y edad. Utilizamos esta información para personalizar su experiencia y proporcionarle contenido relevante según su edad. También recopilamos cierta información automáticamente desde su dispositivo, como la dirección IP, tipo de dispositivo, identificadores únicos de dispositivo e información del navegador.')}
 
-      <p class="text-lg mb-4">Esta Política de Privacidad fue actualizada por última vez el 1 de marzo de 2023.</p>
+      {sectionTitle('Cómo usamos la información')}
+      {sectionContent('Usamos la información recopilada para personalizar su experiencia en la app, enviarle comunicaciones, mejorar nuestros productos y servicios, y analizar el uso de la app. No vendemos ni compartimos su información personal con terceros para marketing o publicidad.')}
 
-      <p class="text-lg mb-4">En Una Vida Brillante, nos comprometemos a proteger su privacidad. Esta Política de Privacidad explica cómo recopilamos, usamos, compartimos y protegemos los datos personales que proporciona en nuestra app.</p>
+      {sectionTitle('Almacenamiento y retención de datos')}
+      {sectionContent('Conservamos sus datos personales el tiempo necesario para proporcionarle nuestros servicios o según lo requiera la ley. Puede solicitarnos que eliminemos su cuenta y todos sus datos asociados escribiendo a hola@unavidabrillante.com.')}
 
-      <h2 class="text-3xl font-bold mb-4">Información que recopilamos</h2>
+      {sectionTitle('Seguridad de los datos')}
+      {sectionContent('Implementamos medidas de seguridad razonables para proteger sus datos e impedir accesos no autorizados. Usamos Firebase para almacenar y proteger la información recopilada. Firebase encripta la información en tránsito y proporciona varias capas de seguridad para proteger los datos.')}
 
-      <p class="text-lg mb-4">Cuando se registra en nuestra app, le solicitamos su nombre, dirección de correo electrónico, ubicación aproximada y edad. Utilizamos esta información para personalizar su experiencia y proporcionarle contenido relevante según su edad.</p>
+      {sectionTitle('Transferencias Internacionales de Datos')}
+      {sectionContent('Los datos que recopilamos se almacenan en servidores de Google Cloud Platform ubicados en Estados Unidos. Google LLC y sus compañías afiliadas cumplen con el Reglamento General de Protección de Datos de la UE (GDPR).')}
 
-      <p class="text-lg mb-4">También recopilamos cierta información automáticamente desde su dispositivo, como la dirección IP, tipo de dispositivo, identificadores únicos de dispositivo e información del navegador.</p>
+      {sectionTitle('Derechos del Usuario bajo el GDPR')}
+      {sectionContent('Si eres residente de la Unión Europea, tienes ciertos derechos bajo el Reglamento General de Protección de Datos (GDPR). Para ejercer cualquiera de estos derechos, contáctanos en hola@unavidabrillante.com.')}
 
-      <h2 class="text-3xl font-bold mb-4">Cómo usamos la información</h2>
+      {sectionTitle('Leyes de Privacidad de Ecuador')}
+      {sectionContent('De acuerdo con las leyes de privacidad de datos de Ecuador, garantizamos que su información personal será tratada con confidencialidad y seguridad, y solo será utilizada para los fines establecidos en esta política.')}
 
-      <p class="text-lg mb-4">Usamos la información recopilada para:</p>
+      {sectionTitle('Desactivación y Eliminación de Cuentas')}
+      {sectionContent('Para desactivar o eliminar su cuenta y todos los datos asociados, visite www.unavidabrillante.com/#/eliminarcuenta y complete el formulario correspondiente.')}
 
-      <ul class="list-disc list-inside mb-4">
-        <li>Personalizar su experiencia en la app y proporcionar contenido relevante.</li>
-        <li>Enviarle comunicaciones como actualizaciones, alertas y mensajes promocionales.</li>
-        <li>Mejorar nuestros productos y servicios.</li>
-        <li>Analizar el uso de la app para mejorarla.</li>
-      </ul>
+      {sectionTitle('Cambios a esta Política de Privacidad')}
+      {sectionContent('Podemos actualizar esta Política de Privacidad periódicamente. Le notificaremos cualquier cambio publicando la nueva Política de Privacidad en esta página. Le recomendamos que revise esta página con regularidad para estar informado sobre cómo estamos protegiendo su información.')}
 
-      <p class="text-lg mb-4">No vendemos ni compartimos su información personal con terceros para marketing o publicidad.</p>
+      {sectionTitle('Contáctenos')}
+      {sectionContent('Si tiene alguna pregunta sobre esta Política de Privacidad, puede contactarnos en hola@unavidabrillante.com.')}
 
-      <h2 class="text-3xl font-bold mb-8">Almacenamiento y retención de datos</h2>
-
-      <p class="text-lg mb-4">Conservamos sus datos personales el tiempo necesario para proporcionarle nuestros servicios o según lo requiera la ley. Puede solicitarnos que eliminemos su cuenta y todos sus datos asociados escribiendo a hola@unavidabrillante.com.</p>
-
-      <h2 class="text-3xl font-bold mb-8">Seguridad de los datos</h2>
-
-      <p class="text-lg mb-4">Implementamos medidas de seguridad razonables para proteger sus datos e impedir accesos no autorizados. Sin embargo, ningún método de transmisión por Internet o método de almacenamiento electrónico es 100% seguro. Usamos Firebase para almacenar y proteger la información recopilada. Firebase encripta la información en tránsito y proporciona varias capas de seguridad para proteger los datos.</p>
-
-      <h2 class="text-3xl font-bold mb-8">Transferencias Internacionales de Datos</h2>
-
-      <p class="text-lg mb-4">Los datos que recopilamos se almacenan en servidores de Google Cloud Platform ubicados en Estados Unidos. Google LLC y sus compañías afiliadas cumplen con el Reglamento General de Protección de Datos de la UE.</p>
-
-      <h2 class="text-3xl font-bold mb-8">Cambios a esta Política de Privacidad</h2>
-
-      <p class="text-lg mb-4">Podemos actualizar esta Política de Privacidad periódicamente. Le notificaremos cualquier cambio publicando la nueva Política de Privacidad en esta página. Le recomendamos que revise esta página con regularidad para estar informado sobre cómo estamos protegiendo su información.</p>
-
-      <h2 class="text-3xl font-bold mb-8">Contáctenos</h2>
-
-      <p class="text-lg">Si tiene alguna pregunta sobre esta Política de Privacidad, puede contactarnos en hola@unavidabrillante.com.</p>
     </div>
   );
 };
 
-export default PoliticaDePrivacidad;
+const sectionTitle = (title) => (
+  <h3 style={{ fontSize: '22px', fontWeight: '600', color: '#E91E63', margin: '12px 0' }}>
+    {title}
+  </h3>
+);
+
+const sectionContent = (content) => (
+  <p style={{ fontSize: '16px', lineHeight: '1.5', marginBottom: '16px' }}>
+    {content}
+  </p>
+);
+
+export default PoliticaPrivacidad;
