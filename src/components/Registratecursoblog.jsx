0eff0933ca3a97204eb13ec6edfe1848538f946b
Paola Suarez <paolasuarez86@gmail.com>
2023-09-14 12:50:38 -0500

    registro cambio

--- src/components/Registratecursoblog.jsx
+++ src/components/Registratecursoblog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Registratecursoblog() {
     return (
-        <div className="bg-strong-pink p-40" id="omnisend-embedded-v2-649a1f2f90789f2631ae66e5"></div>
+        <div className="bg-strong-pink py-40 px-2 md:px-40" id="omnisend-embedded-v2-649a1f2f90789f2631ae66e5"></div>
     );
 }
 export default Registratecursoblog;
